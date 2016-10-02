349cbd2109c7b80efa0934f5da5055c641124c16
caibate <caibate@gmail.com>
2016-10-02 20:25:35 -0300

    Tratamento 404 e 201

--- social-books-api/src/main/java/com/socialbooks/api/resources/LivrosResources.java
+++ social-books-api/src/main/java/com/socialbooks/api/resources/LivrosResources.java
@@ -1,13 +1,17 @@
 package com.socialbooks.api.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.socialbooks.api.entity.Livro;
 import com.socialbooks.api.repository.LivrosRepository;
@@ -25,13 +29,19 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro){
-		livroRepository.save(livro);
+	public ResponseEntity<?> salvar(@RequestBody Livro livro){
+		Livro l = livroRepository.save(livro);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().path("/{id}").
+				buildAndExpand(l.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id){
-		return livroRepository.findOne(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
+		Livro l= livroRepository.findOne(id);
+		if(l == null) return ResponseEntity.notFound().build();
+		return ResponseEntity.status(HttpStatus.OK).body(l);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
@@ -43,5 +53,4 @@ public class LivrosResources {
 		livro.setId(id);
 		livroRepository.save(livro);
 	}
-	
 }
