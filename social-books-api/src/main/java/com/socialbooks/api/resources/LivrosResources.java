de51b6a168358f8a591c53d23859633ecf584bcb
caibate <caibate@gmail.com>
2016-10-02 19:21:58 -0300

    Add post recurso Livro.

--- social-books-api/src/main/java/com/socialbooks/api/resources/LivrosResources.java
+++ social-books-api/src/main/java/com/socialbooks/api/resources/LivrosResources.java
@@ -1,7 +1,9 @@
 package com.socialbooks.api.resources;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,19 @@ import com.socialbooks.api.entity.Livro;
 import com.socialbooks.api.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 	
 	@Autowired
 	private LivrosRepository livroRepository;
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar(){
 		return livroRepository.findAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro){
+		livroRepository.save(livro);
 	}
 }
